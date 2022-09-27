b29931218ff4c9d41ce0d732fed2a0016a3c4719
Ulysse Chopard <chopard.ulysse@gmail.com>
2022-09-27 23:37:20 +0100

    change views

--- api/db/migrations/1664285247759_create-table-users.sql
+++ api/db/migrations/1664285247759_create-table-users.sql
@@ -1,10 +1,12 @@
 -- Up Migration
+CREATE TYPE role AS ENUM ('manager', 'expert');
 CREATE TABLE IF NOT EXISTS users (
-    ID SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    email TEXT,
     password VARCHAR(98) NOT NULL,
-    UNIQUE(password, email)
+    role ROLE,
+    UNIQUE(email, role)
 );
 -- Down Migration
 DROP TABLE users;
+DROP TYPE IF EXISTS role;
--- api/routes/views.js
+++ api/routes/views.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
 
 router.get("/", (_req, res) => res.render("index"));
-router.get("/login", (_req, res) => res.render("login"));
-router.get("/signup", (_req, res) => res.render("signup"));
+router.get("/login", (req, res) =>
+  res.render("login", { role: req.query.role || "expert" })
+);
+router.get("/signup", (req, res) =>
+  res.render("signup", { role: req.query.role })
+);
+router.get("/manager", (_req, res) => res.render("login", { role: "manager" }));
+router.get("/expert", (_req, res) => res.render("login", { role: "expert" }));
 
 module.exports = router;
