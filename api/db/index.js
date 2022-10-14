0ddf5b90946488a1a71843282aadbb18bb7de6e0
Ulysse Chopard <chopard.ulysse@gmail.com>
2022-10-14 21:13:22 +0100

    db connection

--- api/db/index.js
+++ api/db/index.js
@@ -3,7 +3,12 @@ const { Pool } = require("pg");
 
 const pool = new Pool(
   process.env.DATABASE_URL
-    ? { connectionString: process.env.DATABASE_URL }
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
     : null
 );
 
