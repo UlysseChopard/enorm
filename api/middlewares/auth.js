2b25bd9c80c3e6021b093ebc79a34cf72a94ea6c
Ulysse Chopard <chopard.ulysse@gmail.com>
2022-10-17 23:39:00 +0100

    rm useless deps

--- api/middlewares/auth.js
+++ api/middlewares/auth.js
@@ -1,7 +1,7 @@
 const log = require("../utils/logs");
 
 exports.isAuthenticated = (req, res, next) => {
-  const isActivated = req.user.is_activated;
+  const isActivated = req.user?.is_activated;
   log.info("auth", {
     isAuthenticated: req.isAuthenticated(),
     isActivated,
