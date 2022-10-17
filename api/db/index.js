773ab1c9e61c2206eb57c679acd778a91b8ba287
Ulysse Chopard <chopard.ulysse@gmail.com>
2022-10-17 21:49:00 +0100

    Revert "test ssl"
    This reverts commit b3c600362b92a8c7117c0d5cdf92cfc2505f65e5.

--- api/db/index.js
+++ api/db/index.js
@@ -4,7 +4,7 @@ const { Pool } = require("pg");
 const pool = new Pool(
   process.env.DATABASE_URL
     ? {
-        connectionString: process.env.DATABASE_URL + "?sslmode=require",
+        connectionString: process.env.DATABASE_URL,
         ssl: {
           rejectUnauthorized: false,
         },
