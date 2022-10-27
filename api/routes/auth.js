4e42c2a2110a4d8ecaa770546374bf7dfff1de12
ulysse_chopard <ulysse.chopard@ksaar.com>
2022-10-27 21:49:00 +0200

    simplify auth

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,25 +1,17 @@
 const router = require("express").Router();
 const {
+  signup,
   logout,
   login,
   sendUser,
-  activateExpert,
-  activateManager,
-  signupManager,
-  sendEmailConfirmation,
+  sendEmailValidation,
 } = require("../controllers/auth");
 const { isAuthenticated } = require("../middlewares/auth");
 const passport = require("../middlewares/passport");
 
+router.post("/signup", signup, sendEmailValidation);
 router.post("/login", passport.authenticate("local"), login);
-router.get(
-  "/confirm/:uuid",
-  passport.authenticate("hash"),
-  activateManager,
-  login
-);
-router.post("/signup/managers", signupManager, sendEmailConfirmation);
-router.patch("/signup/experts/:uuid", activateExpert);
+router.get("/confirm/:uuid", passport.authenticate("hash"), login);
 router.post("/logout", isAuthenticated, logout);
 router.get("/user", isAuthenticated, sendUser);
 
