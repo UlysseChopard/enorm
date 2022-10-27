7dfb3696b4c2836c5f30114e42b0d6356ef4c9d8
ulysse_chopard <ulysse.chopard@ksaar.com>
2022-10-27 21:30:42 +0200

    add password to account creation api

--- web/src/api/user.js
+++ web/src/api/user.js
@@ -6,11 +6,11 @@ export function getUser() {
   }).then(returnContentOrNull);
 }
 
-export function createAccount({ firstName, lastName, email }) {
+export function createAccount({ firstName, lastName, email, password }) {
   return fetch(import.meta.env.VITE_API_URL + "/signup", {
     credentials: "include",
     headers: { Accept: "application/json", "Content-Type": "application/json" },
-    body: JSON.stringify({ firstName, lastName, email }),
+    body: JSON.stringify({ firstName, lastName, email, password }),
   }).then(returnContentOrNull);
 }
 
