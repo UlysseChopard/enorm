05e5969499716584919bbf9b2753664b8aac321a
ulysse_chopard <ulysse.chopard@ksaar.com>
2022-10-27 21:27:28 +0200

    template root

--- web/src/routes/root.jsx
+++ web/src/routes/root.jsx
@@ -1,5 +1,5 @@
-import React, { useRef } from "react";
-import { redirect, Outlet, useLoaderData } from "react-router-dom";
+import React, { useState } from "react";
+import { redirect, Outlet, useLoaderData, Link } from "react-router-dom";
 import { getUser } from "../api/user.js";
 
 export async function loader() {
@@ -10,24 +10,36 @@ export async function loader() {
 
 const Root = () => {
   const { user } = useLoaderData();
-  const sidebar = useRef(false);
+  const [sidebar, setSidebar] = useState(false);
   console.log("usr", user, sidebar.current);
   return (
-    <>
+    <div id="root">
+      <div
+        id="app-banner"
+        className="h-12 bg-slate-200 flex items-center justify-between"
+      >
+        <div className="mx-4">
+          <button onClick={() => setSidebar(!sidebar)} className="mx-auto">
+            Sidebar
+          </button>
+        </div>
+        <h1 className="text-2xl font-bold">E-norm</h1>
+        <div />
+      </div>
       <div
         id="sidebar"
-        className={`fixed left-0 h-screen bg-slate-100 ${
-          sidebar.current ? "w-32" : ""
+        className={`fixed left-0 h-screen bg-slate-700 text-white flex flex-col p-4 ${
+          sidebar ? "" : "hidden"
         }`}
       >
-        <button onClick={() => (sidebar.current = !sidebar.current)}>
-          Sidebar
-        </button>
+        <Link>Lien 1</Link>
+        <Link>Lien 2</Link>
+        <Link>Lien 3</Link>
       </div>
       <div id="main">
         <Outlet />
       </div>
-    </>
+    </div>
   );
 };
 
