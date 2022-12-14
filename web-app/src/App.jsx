32b6e0b92331e8a8e33c461208a71901598bc6a5
Ulysse Chopard <chopard.ulysse@gmail.com>
2022-12-14 07:27:33 +0000

    web-app

---
+++ web-app/src/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>my app</div>;
+};
+
+export default App;
---
+++ web-app/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {require('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
