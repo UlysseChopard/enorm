6a98606c197a3b60b11e50ed50d0b37a8e3896a5
ulysse_chopard <ulysse.chopard@ksaar.com>
2023-01-02 21:11:08 +0100

    feat: add css baseline

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -7,12 +7,14 @@ import "@/utils/i18n";
 import * as ReactDOM from "react-dom/client";
 import React, { Suspense } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
 import routes from "@/routes";
 
 const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <CssBaseline />
     <Suspense fallback={<p>Loading...</p>}>
       <RouterProvider router={router} />
     </Suspense>
