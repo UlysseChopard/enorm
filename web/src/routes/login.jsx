e860160ff7626abb491cedb83d621eaeeb176299
ulysse_chopard <ulysse.chopard@ksaar.com>
2023-01-03 00:21:08 +0100

    feat: style login

--- web/src/routes/login.jsx
+++ web/src/routes/login.jsx
@@ -1,7 +1,13 @@
-import { Link, redirect, Form } from "react-router-dom";
+import { redirect, Form } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { login } from "@/api/accounts";
-import StyledInput from "@/components/StyledInput";
+import TextField from "@mui/material/TextField";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
 
 export async function action({ request }) {
   const formData = await request.formData();
@@ -13,40 +19,29 @@ export async function action({ request }) {
 const Login = () => {
   const { t } = useTranslation(null, { keyPrefix: "login" });
   return (
-    <div id="login">
-      <div id="presentation">
-        <h1>{t("brandname")}</h1>
-      </div>
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="space-around"
+      minHeight="100vh"
+    >
+      <Stack spacing={2} alignItems="center">
+        <Typography variant="h1">{t("brandname")}</Typography>
+        <Typography variant="subtitle1">Blablabla</Typography>
+      </Stack>
       <Form
         autoComplete="on"
         method="post"
       >
-        <StyledInput
-          type="email"
-          name="email"
-          label={t("email")}
-          autoComplete="email"
-        />
-        <StyledInput
-          type="password"
-          name="password"
-          label={t("password")}
-          autoComplete="current-password"
-        />
-        <button
-          type="submit"
-        >
-          {t("submit")}
-        </button>
-        <Link to="/reset-password">
-          <p>{t("resetPassword")}</p>
-        </Link>
-        <div/>
-        <Link to="/signup">
-          <p>{t("signup")}</p>
-        </Link>
+        <Stack spacing={2} alignItems="center" minWidth="50vw">
+          <TextField variant="filled" label={t("email")} autoComplete="email" fullWidth/>
+          <TextField variant="filled" label={t("password")} autoComplete="current-password" fullWidth/>
+          <Button variant="contained" type="submit" fullWidth>{t("submit")}</Button>
+          <Button variant="text"  href="/signup" fullWidth>{t("signup")}</Button>
+          <Link href="/reset-password" fullWidth>{t("resetPassword")}</Link>
+        </Stack>
       </Form>
-    </div>
+    </Box>
   );
 };
 
