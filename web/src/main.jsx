11e3ff0b4f2d6e104a1f88bda752f4baa52677ed
ulysse_chopard <ulysse.chopard@ksaar.com>
2023-01-02 21:00:39 +0100

    refactor: extract routes from entrypoint

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,108 +1,15 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@/index.css";
+import "@/utils/i18n";
 import * as ReactDOM from "react-dom/client";
 import React, { Suspense } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "@/index.css";
-import "@/utils/i18n";
-import Home, { loader as homeLoader } from "@/routes/home";
-import ErrorPage from "@/routes/error-page";
-import Login, { action as loginAction } from "@/routes/login";
-import Signup, { action as signupAction } from "@/routes/signup";
-import Logout, { loader as logoutLoader } from "@/routes/logout";
-import Activate, { loader as activateLoader } from "@/routes/activate";
-import Account, {
-  loader as accountLoader,
-  action as accountAction,
-} from "@/routes/home/account";
-import SendResetPasswordLink, {
-  action as sendResetPasswordLinkAction,
-} from "@/routes/reset-password/send-link";
-import ResetPassword, {
-  action as resetPasswordAction,
-} from "@/routes/reset-password";
-import Roles, { loader as rolesLoader } from "@/routes/home/roles";
-import DeclareRole, {
-  action as declareRoleAction,
-} from "@/routes/home/roles/declare.jsx";
+import routes from "@/routes";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />,
-    loader: homeLoader,
-    children: [
-      {
-        errorElement: <ErrorPage />,
-        children: [
-          {
-            path: "account",
-            element: <Account />,
-            loader: accountLoader,
-            action: accountAction,
-          },
-          {
-            path: "roles",
-            element: <Roles />,
-            loader: rolesLoader,
-            children: [
-              {
-                path: "declare",
-                element: <DeclareRole />,
-                action: declareRoleAction,
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-  {
-    path: "/login",
-    element: <Login />,
-    errorElement: <ErrorPage />,
-    action: loginAction,
-  },
-  {
-    path: "/reset-password",
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "",
-        element: <SendResetPasswordLink />,
-        action: sendResetPasswordLinkAction,
-      },
-      {
-        path: ":uuid",
-        action: resetPasswordAction,
-        element: <ResetPassword />,
-      },
-    ],
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-    errorElement: <ErrorPage />,
-    action: signupAction,
-  },
-  {
-    path: "/logout",
-    element: <Logout />,
-    errorElement: <ErrorPage />,
-    loader: logoutLoader,
-  },
-  {
-    path: "/activate",
-    element: <Activate />,
-    loader: activateLoader,
-    children: [
-      {
-        path: ":uuid",
-        element: <Activate />,
-        loader: activateLoader,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
---
+++ web/src/routes/index.js
@@ -0,0 +1,100 @@
+import Home, { loader as homeLoader } from "@/routes/home";
+import ErrorPage from "@/routes/error-page";
+import Login, { action as loginAction } from "@/routes/login";
+import Signup, { action as signupAction } from "@/routes/signup";
+import Logout, { loader as logoutLoader } from "@/routes/logout";
+import Activate, { loader as activateLoader } from "@/routes/activate";
+import Account, {
+  loader as accountLoader,
+  action as accountAction,
+} from "@/routes/home/account";
+import SendResetPasswordLink, {
+  action as sendResetPasswordLinkAction,
+} from "@/routes/reset-password/send-link";
+import ResetPassword, {
+  action as resetPasswordAction,
+} from "@/routes/reset-password";
+import Roles, { loader as rolesLoader } from "@/routes/home/roles";
+import DeclareRole, {
+  action as declareRoleAction,
+} from "@/routes/home/roles/declare.jsx";
+
+export default [
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+    loader: homeLoader,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            path: "account",
+            element: <Account />,
+            loader: accountLoader,
+            action: accountAction,
+          },
+          {
+            path: "roles",
+            element: <Roles />,
+            loader: rolesLoader,
+            children: [
+              {
+                path: "declare",
+                element: <DeclareRole />,
+                action: declareRoleAction,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <ErrorPage />,
+    action: loginAction,
+  },
+  {
+    path: "/reset-password",
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "",
+        element: <SendResetPasswordLink />,
+        action: sendResetPasswordLinkAction,
+      },
+      {
+        path: ":uuid",
+        action: resetPasswordAction,
+        element: <ResetPassword />,
+      },
+    ],
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+    errorElement: <ErrorPage />,
+    action: signupAction,
+  },
+  {
+    path: "/logout",
+    element: <Logout />,
+    errorElement: <ErrorPage />,
+    loader: logoutLoader,
+  },
+  {
+    path: "/activate",
+    element: <Activate />,
+    loader: activateLoader,
+    children: [
+      {
+        path: ":uuid",
+        element: <Activate />,
+        loader: activateLoader,
+      },
+    ],
+  },
+];
