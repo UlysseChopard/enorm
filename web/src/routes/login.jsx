39d2adde4ca975e518461c6c4f3751073291a97b
ulysse_chopard <ulysse.chopard@ksaar.com>
2023-01-03 22:10:20 +0100

    feat: add signup modal

--- web/src/routes/login.jsx
+++ web/src/routes/login.jsx
@@ -1,3 +1,4 @@
+import { useState, useRef } from "react";
 import { redirect, Form } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { login } from "@/api/accounts";
@@ -7,16 +8,69 @@ import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 import Box from "@mui/material/Box";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 export async function action({ request }) {
   const formData = await request.formData();
+  console.log(formData);
   const userInfos = Object.fromEntries(formData);
   const res = await login(userInfos);
   if (res.ok) return redirect("/");
 }
 
+const SignupDialog = ({ onSubmit, onClose, open }) => {
+  const { t } = useTranslation(null, { keyPrefix: "signup" });
+  return (
+    <Dialog onClose={onClose} open={open} fullWidth maxWidth="sm">
+      <DialogTitle>{t("title")}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>{t("text")}</DialogContentText>
+        <Stack spacing={2}>
+          <TextField
+            autoFocus
+            label={t("firstname")}
+            name="firstname"
+            fullWidth
+          />
+          <TextField
+            label={t("lastname")}
+            name="lastname"
+            fullWidth
+          />
+          <TextField
+            label={t("email")}
+            name="email"
+            type="email"
+            fullWidth
+          />
+          <TextField
+            label={t("password")}
+            name="password"
+            type="password"
+            autoComplete="new-password"
+            fullWidth
+          />
+        </Stack>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>{t("cancel")}</Button>
+        <Button onClick={onSubmit}>{t("submit")}</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
 const Login = () => {
+  const [open, setOpen] = useState(false);
+  const formRef = useRef(null);
   const { t } = useTranslation(null, { keyPrefix: "login" });
+
+  const handleSubmit = () => formRef.current.submit();
+
   return (
     <Box
       display="flex"
@@ -26,20 +80,24 @@ const Login = () => {
     >
       <Stack spacing={2} alignItems="center">
         <Typography variant="h1">{t("brandname")}</Typography>
-        <Typography variant="subtitle1">Blablabla</Typography>
+        <Typography variant="subtitle1">{t("presentation")}</Typography>
       </Stack>
       <Form
         autoComplete="on"
         method="post"
+        ref={formRef} 
       >
-        <Stack spacing={2} alignItems="center" minWidth="50vw">
-          <TextField variant="filled" label={t("email")} autoComplete="email" fullWidth/>
-          <TextField variant="filled" type="password" label={t("password")} autoComplete="current-password" fullWidth/>
+        <Stack spacing={2} alignItems="center" width="400px">
+          <TextField variant="filled" label={t("email")} autoComplete="email" type="email" fullWidth/>
+          <Stack width="400px" alignItems="flex-end">
+            <TextField variant="filled" type="password" label={t("password")} autoComplete="current-password" fullWidth/>
+            <Link href="/reset-password">{t("resetPassword")}</Link>
+          </Stack>
           <Button variant="contained" type="submit" fullWidth>{t("submit")}</Button>
-          <Button variant="text" href="/signup" fullWidth>{t("signup")}</Button>
-          <Link href="/reset-password">{t("resetPassword")}</Link>
+          <Button variant="text" onClick={() => setOpen(true)} fullWidth>{t("signup")}</Button>
         </Stack>
       </Form>
+      <SignupDialog open={open} onClose={() => setOpen(false)} onSubmit={handleSubmit} />
     </Box>
   );
 };
