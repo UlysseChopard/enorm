5e2b0693930aed94adcc7179f483b289330b07f2
ulysse_chopard <ulysse.chopard@ksaar.com>
2023-01-10 23:01:21 +0100

    fix: cannot share express router between routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,8 +2,8 @@ const sessions = require("./sessions");
 const accounts = require("./accounts");
 
 module.exports = (express, app) => {
-  const router = express.Router();
+  const router = express.Router;
 
-  app.use("/api/sessions", sessions(router));
-  app.use("/api/accounts", accounts(router));
+  app.use("/api/sessions", sessions(router()));
+  app.use("/api/accounts", accounts(router()));
 };
