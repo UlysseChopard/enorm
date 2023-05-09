02394d012126c77da3f684d8becada0f037cf177
ulysse_chopard <ulysse.chopard@ksaar.com>
2023-05-09 21:50:32 +0200

    fix: db query for received registration request

--- api/models/registrations.js
+++ api/models/registrations.js
@@ -5,7 +5,7 @@ exports.getSended = (userId) =>
 
 exports.getReceived = (userId) =>
   db.query(
-    "SELECT r.* FROM registrations_streams AS rs JOIN registrations ON rs.registration = r.id WHERE rs.subscription IN (SELECT s.id FROM subscriptions AS s WHERE s.recipient = $1)",
+    "SELECT r.* FROM registrations_streams AS rs JOIN registrations AS r ON rs.registration = r.id WHERE rs.wg_path IN (SELECT wgp.id FROM wg_paths AS wgp JOIN subscriptions AS s ON wgp.subscription = s.id  WHERE s.recipient = $1)",
     [userId]
   );
 
