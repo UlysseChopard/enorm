ba9243a6dd8e223824f9b9181e63a37f6ec12633
ulysse_chopard <ulysse.chopard@ksaar.com>
2023-05-10 22:30:49 +0200

    save

--- api/controllers/working-groups.js
+++ api/controllers/working-groups.js
@@ -1,8 +1,25 @@
-const { WorkingGroups } = require("../models");
+const { WorkingGroups, WGPaths, Subscriptions } = require("../models");
+
+const propagate = async (
+  userId,
+  wg,
+  subscription = null,
+  propagated = new Set()
+) => {
+  if (propagated.has(subscription)) return;
+  if (subscription) {
+    await WGPaths.add(subscription, wg);
+  }
+  const { rows: subscriptions } = await Subscriptions.getSubscribers(userId);
+  for (const subscription of subscriptions) {
+    propagated.add(subscription.id);
+    await propagate(subscription.sender, wg, subscription.id, propagated);
+  }
+};
 
 exports.get = async (_req, res, next) => {
   try {
-    const { rows: groups } = await WorkingGroups.getByUserId(res.locals.userId);
+    const { rows: groups } = await WorkingGroups.getByUser(res.locals.userId);
     res.json({ groups });
   } catch (err) {
     next(err);
@@ -11,11 +28,22 @@ exports.get = async (_req, res, next) => {
 
 exports.create = async (req, res, next) => {
   try {
-    const { group } = req.body;
     const {
-      rows: [newGroup],
-    } = await WorkingGroups.create(res.locals.userId, group);
-    res.status(201).json({ group: newGroup });
+      rows: [group],
+    } = await WorkingGroups.create(res.locals.userId, req.body.group);
+    await propagate(res.locals.userId, group.id);
+    res.status(201).json({ group });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.find = async (req, res, next) => {
+  try {
+    const {
+      rows: [wg],
+    } = await WorkingGroups.find(res.locals.userId, req.params.id);
+    res.json({ wg });
   } catch (err) {
     next(err);
   }
--- api/models/wg-paths.js
+++ api/models/wg-paths.js
@@ -5,3 +5,9 @@ exports.propagate = ({ subscription, recipient }) =>
     "INSERT INTO wg_paths (subscription, working_group) SELECT $1::INTEGER, id FROM working_groups WHERE admin = $2 UNION SELECT $1::INTEGER, working_group FROM wg_paths WHERE subscription IN (SELECT id FROM subscriptions WHERE sender = $2)",
     [subscription, recipient]
   );
+
+exports.add = (subscription, wg) =>
+  db.query(
+    "INSERT INTO wg_paths (subscription, working_group) VALUES ($1, $2)",
+    [subscription, wg]
+  );
