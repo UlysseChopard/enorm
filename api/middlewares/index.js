860bf3994de044a26895b743c92e1126d30c8461
ulysse_chopard <ulysse.chopard@ksaar.com>
2023-06-02 23:54:40 +0200

    fix: add cors on options

--- api/middlewares/index.js
+++ api/middlewares/index.js
@@ -6,15 +6,21 @@ const logger = require("./logger");
 const errorHandler = require("./error-handler");
 const authenticate = require("./auth");
 
+const CORS_OPTIONS = {
+  origin: (origin, cb) => cb(null, true),
+  credentials: true,
+};
+
 exports.preMiddlewares = (express, app) => {
   app.use(compression());
   app.use(helmet());
   app.use(cookieParser());
-  app.use(cors({ origin: (origin, cb) => cb(null, true), credentials: true }));
+  app.use(cors(CORS_OPTIONS));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(logger);
   app.use(authenticate);
+  app.options("*", cors(CORS_OPTIONS));
 };
 
 exports.postMiddlewares = (_express, app) => {
