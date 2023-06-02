3c99c810c4f0a82a366980dc4ab0dfddd617d2a6
ulysse_chopard <ulysse.chopard@ksaar.com>
2023-06-02 22:58:52 +0200

    feat: cors

--- api/middlewares/index.js
+++ api/middlewares/index.js
@@ -1,5 +1,6 @@
 const compression = require("compression");
 const helmet = require("helmet");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const logger = require("./logger");
 const errorHandler = require("./error-handler");
@@ -9,6 +10,7 @@ exports.preMiddlewares = (express, app) => {
   app.use(compression());
   app.use(helmet());
   app.use(cookieParser());
+  app.use(cors({ origin: (origin, cb) => cb(null, true), credentials: true }));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(logger);
