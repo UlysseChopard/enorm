b0a359fe44757e6c1391aecc8f77a3cd93845f5c
ulysse_chopard <ulysse.chopard@ksaar.com>
2023-07-05 22:32:28 +0200

    feat: parse csv

--- api/controllers/administration.js
+++ api/controllers/administration.js
@@ -1,14 +1,40 @@
 const { createReadStream } = require("fs");
+const { unlink } = require("fs/promises");
+const csvParser = require("csv-parser");
 
 // const { Users, Roles } = require("../models");
 
 exports.linkUsers = async (req, res, next) => {
   try {
-    console.log(req.file);
-    createReadStream(req.file.path, { encoding: "utf-8" })
-      .on("data", console.log)
-      .on("error", console.error);
-    res.sendStatus(204);
+    let error = false;
+    createReadStream(req.file.path, {
+      encoding: "utf-8",
+      emitClose: true,
+    })
+      .pipe(
+        csvParser({
+          strict: false,
+          separator: req.query?.separator ?? "|",
+          mapHeaders: ({ header }) => header.toLowerCase(),
+          skipLines: 0,
+        })
+      )
+      .on("headers", (headers) => {
+        if (!headers.includes("email_txt")) {
+          error = true;
+          res.status(422).json({ message: "Missing email header" });
+        }
+      })
+      .on("error", console.error)
+      .on("data", (data) => {
+        console.log(data.email_txt);
+      })
+      .on("close", async () => {
+        await unlink(req.file.path);
+        if (!error) {
+          res.sendStatus(204);
+        }
+      });
   } catch (err) {
     next(err);
   }
