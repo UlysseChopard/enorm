bd56f64ec0294478914baa69617d1f1bcbf592c2
ulysse_chopard <ulysse.chopard@ksaar.com>
2023-07-13 22:27:55 +0200

    feat: add nvm to start script

--- start-dev.js
+++ start-dev.js
@@ -1,7 +1,13 @@
 const { spawn } = require("child_process");
 
-spawn("docker compose up db", { stdio: "inherit", shell: true });
+const nvm = spawn(`${process.env.HOME}/.nvm/nvm.sh use`, {
+  stdio: "inherit",
+  shell: true,
+});
+nvm.on("close", () => {
+  spawn("docker compose up db", { stdio: "inherit", shell: true });
 
-spawn("npm run -w api dev", { stdio: "inherit", shell: true });
+  spawn("npm run -w api dev", { stdio: "inherit", shell: true });
 
-spawn("npm run -w app dev", { stdio: "inherit", shell: true });
+  spawn("npm run -w app dev", { stdio: "inherit", shell: true });
+});
