fed188b3541516be09d55b9fbf6ee04803f30848
ulysse_chopard <ulysse.chopard@ksaar.com>
2023-07-27 22:44:07 +0200

    fix: remove non functional nvm use in start script

--- start-dev.js
+++ start-dev.js
@@ -1,13 +1,7 @@
 const { spawn } = require("child_process");
 
-const nvm = spawn(`${process.env.HOME}/.nvm/nvm.sh use`, {
-  stdio: "inherit",
-  shell: true,
-});
-nvm.on("close", () => {
-  spawn("docker compose up db", { stdio: "inherit", shell: true });
+spawn("docker compose up db", { stdio: "inherit", shell: true });
 
-  spawn("npm run -w api dev", { stdio: "inherit", shell: true });
+spawn("npm run -w api dev", { stdio: "inherit", shell: true });
 
-  spawn("npm run -w app dev", { stdio: "inherit", shell: true });
-});
+spawn("npm run -w app dev", { stdio: "inherit", shell: true });
