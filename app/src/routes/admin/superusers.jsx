5ae222140917d7d33241cc66fb8ac412c652b700
ulysse_chopard <ulysse.chopard@ksaar.com>
2023-10-03 22:11:17 +0200

    feat: front

--- app/src/routes/admin/superusers.jsx
+++ app/src/routes/admin/superusers.jsx
@@ -1,5 +1,14 @@
-import { useLoaderData } from "react-router-dom";
-// import { useTranslation } from "react-i18next";
+import { useLoaderData, useSubmit, Form } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import MenuItem from "@mui/material/MenuItem";
+import Stack from "@mui/material/Stack";
 
 import { get, create, remove } from "@/api/admin/superusers";
 
@@ -25,9 +34,91 @@ export const action = async ({ request }) => {
 };
 
 const Superusers = () => {
-  // const { t } = useTranslation(null, { keyPrefix: "superusers" });
+  const { t } = useTranslation(null, { keyPrefix: "superusers" });
+  const submit = useSubmit();
+  const [open, setOpen] = useState(false);
   const { superusers } = useLoaderData();
-  return JSON.stringify(superusers);
+  return (
+    <>
+      <Button onClick={() => setOpen(true)}>{t("create")}</Button>
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>{t("createTitle")}</DialogTitle>
+        <Form method="POST">
+          <DialogContent>
+            <Stack>
+              <input type="hidden" name="type" value="create" />
+              <TextField
+                autoComplete="email"
+                name="email"
+                type="email"
+                label={t("email")}
+                required
+              />
+              <TextField name="firstname" label={t("firstname")} required />
+              <TextField
+                autoComplete="family-name"
+                name="lastname"
+                label={t("lastname")}
+                required
+              />
+              <TextField
+                name="gender"
+                label={t("gender")}
+                required
+                autoComplete="sex"
+                defaultValue="male"
+                select
+              >
+                <MenuItem value="male">{t("male")}</MenuItem>
+                <MenuItem value="female">{t("female")}</MenuItem>
+              </TextField>
+              <TextField
+                type="password"
+                name="password"
+                label={t("password")}
+                autoComplete="new-password"
+              />
+            </Stack>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => setOpen(false)}>{t("cancel")}</Button>
+            <Button variant="contained" type="submit">
+              {t("submit")}
+            </Button>
+          </DialogActions>
+        </Form>
+      </Dialog>
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">{t("email")}</th>
+            <th scope="col">{t("name")}</th>
+          </tr>
+        </thead>
+        <tbody>
+          {superusers.map(({ id, email, firstname, lastname }) => {
+            const formData = new FormData();
+            formData.append("id", id);
+            formData.append("type", "remove");
+            return (
+              <tr key={id}>
+                <th scope="row">{email}</th>
+                <td>{`${firstname} ${lastname}`}</td>
+                <td>
+                  <Button
+                    type="submit"
+                    onClick={() => submit(formData, { method: "POST" })}
+                  >
+                    {t("remove")}
+                  </Button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default Superusers;
