09080a2850207da0953d6941a1420873e6011c3c
ulysse_chopard <ulysse.chopard@ksaar.com>
2023-11-17 21:57:23 +0100

    feat: simplify group page

--- app/src/routes/home/group.jsx
+++ app/src/routes/home/group.jsx
@@ -1,19 +1,10 @@
-import { useState, useEffect } from "react";
-import {
-  useSubmit,
-  useLoaderData,
-  useActionData,
-  redirect,
-} from "react-router-dom";
+import { useSubmit, useLoaderData, redirect } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
-import SelectProvider from "@/components/SelectProvider";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Typography from "@mui/material/Typography";
-import { request as requestRegistration } from "@/api/organisations/registrations";
 import { find, remove } from "@/api/organisations/working-groups";
 
 export const loader = async ({ params }) => {
@@ -25,9 +16,6 @@ export const action = async ({ params, request }) => {
   const formData = await request.formData();
   let res;
   switch (formData.get("type")) {
-    case "register":
-      res = await requestRegistration({ wgPath: formData.get("wgPath") });
-      return res.json();
     case "delete":
       res = await remove(params.id);
       return res.ok ? redirect("/groups") : res.json();
@@ -36,38 +24,23 @@ export const action = async ({ params, request }) => {
 
 const Group = () => {
   const { wg } = useLoaderData();
-  const actionData = useActionData();
   const submit = useSubmit();
   const { t } = useTranslation(null, { keyPrefix: "group" });
-  const [wgPath, setWgPath] = useState("");
-  const { isAdmin, isManager } = JSON.parse(localStorage.getItem("roles"));
-  useEffect(() => {
-    if (wg.wgPaths.length == 1) {
-      setWgPath(wg.wgPaths[0].id);
-    }
-  }, [wg]);
-  const handleSubmitRegister = () => {
-    const formData = new FormData();
-    formData.append("type", "register");
-    formData.append("wgPath", wgPath);
-    submit(formData, { method: "post" });
-  };
-  const handleSubmitDelete = () => {
+  const { isAdmin } = JSON.parse(localStorage.getItem("roles"));
+  const organisation = parseInt(localStorage.getItem("organisation"));
+  const handleClick = () => {
     const formData = new FormData();
     formData.append("type", "delete");
-    submit(formData, { method: "delete" });
+    submit(formData, { method: "DELETE" });
   };
   return (
-    <Grid container spacing={2}>
-      <Grid item>
+    <div>
+      <div>
         <Button variant="outlined" to="/groups">
           {t("back")}
         </Button>
-      </Grid>
-      <Grid item>
-        <div>{JSON.stringify(actionData, null, 4)}</div>
-      </Grid>
-      <Grid item>
+      </div>
+      <div>
         <Card>
           <CardContent>
             <Typography variant="h2" gutterBottom>
@@ -81,32 +54,14 @@ const Group = () => {
               wg.created_at
             ).toLocaleDateString()}`}</Typography>
           </CardContent>
-          <CardActions>
-            {wg.wgPaths.length !== 0 && (
-              <>
-                <SelectProvider
-                  wgPaths={wg.wgPaths}
-                  onChange={(e) => setWgPath(e.target.value)}
-                  value={wgPath}
-                />
-                <Button
-                  variant="contained"
-                  disabled={!wgPath}
-                  onClick={handleSubmitRegister}
-                >
-                  {t("join")}
-                </Button>
-              </>
-            )}
-            {(isAdmin || isManager) && (
-              <Button variant="contained" onClick={handleSubmitDelete}>
-                {t("delete")}
-              </Button>
-            )}
-          </CardActions>
+          {isAdmin && wg.organisation === organisation && (
+            <CardActions>
+              <Button onClick={handleClick}>{t("delete")}</Button>
+            </CardActions>
+          )}
         </Card>
-      </Grid>
-    </Grid>
+      </div>
+    </div>
   );
 };
 
