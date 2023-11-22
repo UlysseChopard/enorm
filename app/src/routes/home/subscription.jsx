ffaff17d109c4c601b82571a27a7991d262c71ba
ulysse_chopard <ulysse.chopard@ksaar.com>
2023-11-22 20:53:37 +0100

    feat: display subscription informations

--- app/src/routes/home/subscription.jsx
+++ app/src/routes/home/subscription.jsx
@@ -1,5 +1,11 @@
+import { useTranslation } from "react-i18next";
 import { useLoaderData } from "react-router-dom";
 import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 import { find } from "@/api/organisations/subscriptions";
 import { get } from "@/api/organisations/members";
 
@@ -20,13 +26,36 @@ export async function action({ request }) {
 
 export default function Subscription() {
   const { subscription, members } = useLoaderData();
+  const { t } = useTranslation(null, { keyPrefix: "subscription" });
   return (
-    <div>
-      <Button type="button" href="/subscriptions">
+    <Container>
+      <Button
+        type="button"
+        href="/subscriptions"
+        sx={{ position: "relative", top: 50 }}
+      >
         Back
       </Button>
+      <Typography variant="h2" sx={{ textAlign: "center" }}>
+        {localStorage.getItem("organisation") == subscription.sender
+          ? subscription.recipient_name
+          : subscription.sender_name}
+      </Typography>
+      <Paper>
+        <List>
+          <ListItem>{`${t("sentAt")}: ${new Date(
+            subscription.sent_at
+          ).toLocaleString()}`}</ListItem>
+          <ListItem>{`${t("receivedAt")}: ${new Date(
+            subscription.received_at
+          ).toLocaleString()}`}</ListItem>
+          <ListItem>{`${t("acceptedAt")}: ${new Date(
+            subscription.accepted_at
+          ).toLocaleString()}`}</ListItem>
+        </List>
+      </Paper>
       <p>{JSON.stringify(subscription)}</p>
       <p>{JSON.stringify(members)}</p>
-    </div>
+    </Container>
   );
 }
