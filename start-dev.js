c6a0809be088b63f0920113d91b9e47221422600
ulysse_chopard <ulysse.chopard@ksaar.com>
2023-12-04 21:06:47 +0100

    chore: improve start-dev script

--- start-dev.js
+++ start-dev.js
@@ -1,7 +1,13 @@
-const { spawn } = require("child_process");
+const { spawn, exec } = require("child_process");
 
-spawn("docker compose up db", { stdio: "inherit", shell: true });
+spawn("docker compose up db", { stdio: "inherit", shell: true }).on(
+  "close",
+  () => exec("docker compose down")
+);
 
 spawn("npm run -w api dev", { stdio: "inherit", shell: true });
 
 spawn("npm run -w app dev", { stdio: "inherit", shell: true });
+
+process.on("SIGINT", () => console.log("SIGINT"));
+process.on("SIGTERM", () => console.log("SIGTERM"));
