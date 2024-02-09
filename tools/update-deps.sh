5a43d6233dafafb5370cd9354303ffe0f03cc053
ulysse_chopard <ulysse.chopard@ksaar.com>
2024-02-09 23:25:33 +0100

    feat: improve update tool

--- tools/update-deps.sh
+++ tools/update-deps.sh
@@ -1,5 +1,19 @@
-#! /usr/bin/env bash
+#! /usr/bin/env zsh
 
+source ~/.zshrc
+if [ $(nvm version-remote --lts) != $(nvm current) ]; then
+  nvm install --reinstall-packages-from=current --latest-npm --lts
+  npm pkg set engines.node=$(nvm current) engines.npm=$(echo v$(npm --version))
+  git add package.json
+  git commit -m "chore: update node and npm"
+else
+  latest_npm=$(nvm install-latest-npm | tail -1 | awk '{ print $NF }')
+  if [ $latest_npm != $(jq '.engines.npm' package.json) ]; then
+    npm pkg set engines.npm=$latest_npm
+    git add package.json
+    git commit -m "chore: update npm"
+  fi
+fi
 npm update
 if [ $(git diff --name-only | wc -l) -ne 0 ]; then
   git add package*.json
