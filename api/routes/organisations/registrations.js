1603986d1fdba706a12c3a7bef6d86b72f39024d
ulysse_chopard <ulysse.chopard@ksaar.com>
2024-02-10 19:37:52 +0100

    feat: allow self management for registration deletion

--- api/routes/organisations/registrations.js
+++ api/routes/organisations/registrations.js
@@ -8,28 +8,32 @@ const {
   find,
 } = require("controllers/organisations/registrations");
 const { hasRole } = require("middlewares/roles");
-const isSubscriptionManager = async (req, res, next) => {
-  try {
-    if (!req.params.id && !req.body.wgPath) {
-      return res
-        .status(400)
-        .json({ message: "Missing params id or body wgPath" });
-    }
-    const { rows: subscriptionManagers } = req.params.id
-      ? await SubscriptionsManagers.getByRegistration(
-          req.params.id,
-          req.params.organisation,
+
+const isSubscriptionManager =
+  ({ allowSelfManagement = false }) =>
+  async (req, res, next) => {
+    try {
+      if (!req.params.id && !req.body.wgPath) {
+        return res
+          .status(400)
+          .json({ message: "Missing params id or body wgPath" });
+      }
+      const { rows: subscriptionManagers } = req.params.id
+        ? await SubscriptionsManagers.getByRegistration(
+            req.params.id,
+            req.params.organisation,
+          )
+        : await SubscriptionsManagers.getByWgPath(
+            req.body.wgPath,
+            req.params.organisation,
+          );
+      if (
+        subscriptionManagers.find(
+          ({ account }) => account === res.locals.accountId,
         )
-      : await SubscriptionsManagers.getByWgPath(
-          req.body.wgPath,
-          req.params.organisation,
-        );
-    console.log(subscriptionManagers);
-    if (
-      !subscriptionManagers.find(
-        ({ account }) => account === res.locals.accountId,
-      )
-    ) {
+      ) {
+        next();
+      }
       if (req.params.id) {
         const { rows } = await Registrations.isOrganisationManager(
           req.params.organisation,
@@ -37,15 +41,22 @@ const isSubscriptionManager = async (req, res, next) => {
           req.params.id,
         );
         if (rows) next();
+        if (allowSelfManagement) {
+          const {
+            rows: [registration],
+          } = await Registrations.find(req.params.id);
+          if (registration.beneficiary === res.locals.accountId) next();
+        }
+      }
+      if (allowSelfManagement) {
+        throw new Error("allowSelfManagement must be used with req.params.id");
       }
       const message = "Only allowed to subscription managers";
       return res.status(403).json({ message });
+    } catch (err) {
+      next(err);
     }
-    next();
-  } catch (err) {
-    next(err);
-  }
-};
+  };
 
 module.exports = ({ Router }) => {
   const router = Router({ mergeParams: true });
@@ -55,19 +66,19 @@ module.exports = ({ Router }) => {
   router.patch(
     "/:id",
     hasRole("admin", "manager"),
-    isSubscriptionManager,
+    isSubscriptionManager(),
     forward,
   );
   router.delete(
     "/:id",
-    hasRole("admin", "manager"),
-    isSubscriptionManager,
+    hasRole("admin", "manager", "expert"),
+    isSubscriptionManager({ allowSelfManagement: true }),
     deny,
   );
   router.post(
     "/:id",
     hasRole("admin", "manager"),
-    isSubscriptionManager,
+    isSubscriptionManager(),
     accept,
   );
   return router;
