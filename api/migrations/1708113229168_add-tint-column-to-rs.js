9b517adb856ac5a3e6d3aecf558aa3bc97491c5d
ulysse_chopard <ulysse.chopard@gmail.com>
2024-02-16 21:20:45 +0100

    feat: improve migration

--- api/migrations/1708113229168_add-tint-column-to-rs.js
+++ api/migrations/1708113229168_add-tint-column-to-rs.js
@@ -2,10 +2,14 @@
 
 exports.shorthands = undefined;
 
-exports.up = (pgm) => {
+exports.up = async (pgm) => {
   pgm.addColumn("registrations_streams", {
     tint: { type: "integer", references: "organisations" },
   });
+  pgm.sql(
+    "UPDATE registrations_streams SET tint = om.organisation FROM registrations_streams rs JOIN registrations r ON rs.registration = r.id JOIN organisations_members om ON r.beneficiary = om.account",
+  );
+  pgm.alterColumn("registrations_streams", "tint", { notNull: true });
 };
 
 exports.down = (pgm) => {
