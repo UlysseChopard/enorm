bc838056f6500b0dabeef59d5d78b0f217bae0a9
ulysse_chopard <ulysse.chopard@gmail.com>
2024-02-29 19:10:55 +0100

    chore: add eslint plugin for mocha

--- api/utils/log.js
+++ api/utils/log.js
@@ -1,3 +1,11 @@
+const { createWriteStream } = require("node:fs");
 const { createLogger, stdSerializers: serializers } = require("bunyan");
 
-module.exports = createLogger({ name: "Jadoube API", serializers });
+module.exports = createLogger({
+  name: "Jadoube API",
+  stream:
+    process.env.NODE_ENV !== "test"
+      ? process.stdout
+      : createWriteStream("/dev/null"),
+  serializers,
+});
