4e2191af999ed0960c47a2127a2e8635d20640fb
ulysse_chopard <ulysse.chopard@gmail.com>
2024-02-29 19:33:49 +0100

    tests: first working

--- api/tests/accounts.test.js
+++ api/tests/accounts.test.js
@@ -6,9 +6,10 @@ const app = require("app");
 chai.use(chaiHTTP);
 
 describe("Test /accounts", () => {
-  describe("Test GET /", () => {
-    it("should return 401 when not logged in", async () => {
-      const res = await chai.request(app).get("/accounts");
+  describe("Test GET /:id", () => {
+    it("should return 401 when not authenticated", async () => {
+      const res = await chai.request(app).get("/api/accounts/0");
+      console.log(res);
       expect(res).to.have.status(401);
     });
   });
