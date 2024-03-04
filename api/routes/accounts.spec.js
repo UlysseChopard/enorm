5c5a07e9823f716b6dcbbde8776496900d356e07
ulysse_chopard <ulysse.chopard@gmail.com>
2024-03-04 22:34:37 +0100

    save

--- api/routes/accounts.spec.js
+++ api/routes/accounts.spec.js
@@ -1,69 +1,67 @@
 const chai = require("chai");
 const chaiHTTP = require("chai-http");
-const sinon = require("sinon");
+const sinon = require("sinon").createSandbox();
 const rewire = require("rewire");
-const accountsController = require("controllers/accounts");
+// const accountsController = require("controllers/accounts");
 
+const app = rewire("app");
 const routes = rewire("routes");
-const accountsRouter = rewire("routes/accounts");
+const accountsRoutes = rewire("routes/accounts");
 
 const expect = chai.expect;
 
-const replaceModule = (path, newModule) => {
-  require.cache[require.resolve(path)] = { exports: newModule };
-};
-
-const passAuthentication = () => {
-  const resetRoutes = routes.__set__("isAuthenticated", (_req, _res, next) =>
-    next(),
-  );
-  replaceModule("routes", routes);
-  return resetRoutes;
-};
-
-const isAccountOwner = () => {
-  const resetAccountsRouter = accountsRouter.__set__(
-    "isAccountOwner",
-    (_req, _res, next) => next(),
-  );
-  replaceModule("routes/accounts", accountsRouter);
-  return resetAccountsRouter;
-};
-
 chai.use(chaiHTTP);
+
+const requireStubbed = (path, stubs) => {
+  if (require.cache[require.resolve(path)]) {
+    delete require.cache[require.resolve(path)];
+  }
+  Object.entries(stubs).forEach(([stubPath, stub]) => {
+    require.cache[require.resolve(stubPath)] = {
+      exports: stub,
+    };
+  });
+  return require(path);
+};
+
+const middlewareStub = sinon.stub().callsFake((_req, _res, next) => next());
+
+afterEach(() => {
+  sinon.restore();
+});
 
 describe("Test /accounts", () => {
   describe("Test GET /:id", () => {
-    beforeEach(() => {
-      delete require.cache[require.resolve("app")];
-    });
-
     it("should return 401 when not authenticated", async () => {
-      const app = require("app");
       const res = await chai.request(app).get("/api/accounts/0");
       expect(res).to.have.status(401);
     });
 
+    const restoreRoutes = routes.__set__("isAuthenticated", middlewareStub);
+
     it("should return 403 when not account owner", async () => {
-      const resetRoutes = passAuthentication();
-      const app = require("app");
+      const app = requireStubbed("app", { routes });
       const res = await chai.request(app).get("/api/accounts/0");
       expect(res).to.have.status(403);
-      resetRoutes();
     });
 
+    const restoreAccountsRoutes = accountsRoutes.__set__(
+      "isAccountOwner",
+      middlewareStub,
+    );
+
     it("should call get when account owner", async () => {
-      const resetRoutes = passAuthentication();
-      const resetAccountsRouter = isAccountOwner();
-      replaceModule("controllers/accounts", {
-        ...accountsController,
-        get: (_req, res) => res.sendStatus(204),
+      const restore = accountsRoutes.__set__("get", (_req, res, _next) =>
+        res.sendStatus(204),
+      );
+      const app = requireStubbed("app", {
+        routes,
+        "routes/accounts": accountsRoutes,
       });
-      const app = require("app");
       const res = await chai.request(app).get("/api/accounts/0");
+      console.log(res.body);
       expect(res).to.have.status(204);
-      resetAccountsRouter();
-      resetRoutes();
+      restore();
     });
   });
 });
